7d41adbd150a8ecda39d2f553fae9c5ef32261aa
Arkadiusz Pień <a.pien@newmotion.com>
2025-04-06 19:22:08 +0200

    Use Scala 3 and upgrade of dependencies.

--- client/src/main/scala/configuration/Configuration.scala
+++ client/src/main/scala/configuration/Configuration.scala
@@ -3,7 +3,8 @@ package configuration
 import com.typesafe.config.Config
 import pureconfig.ConfigObjectSource
 import pureconfig.ConfigSource
-import pureconfig.generic.auto._
+import pureconfig._
+import pureconfig.generic.derivation.default._
 
 case class Configuration(
   kafka: KafkaConfiguration
@@ -14,6 +15,8 @@ case class KafkaConfiguration(
   group: String,
   client: String
 )
+
+given ConfigReader[Configuration] = ConfigReader.derived
 
 object Configuration {
   def apply(config: Config): Configuration = {
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,18 +2,23 @@ import sbt._
 import sbt.librarymanagement.ModuleID
 
 object Dependencies {
+  private val tapirVersion = "1.11.23"
 
   private val config = Seq(
-    "com.typesafe"           % "config"     % "1.4.3",
-    "com.github.pureconfig" %% "pureconfig" % "0.17.6"
+    "com.typesafe"           % "config"          % "1.4.3",
+    "com.github.pureconfig" %% "pureconfig-core" % "0.17.8"
   )
 
   lazy val zio: Seq[ModuleID] = Seq(
-    "dev.zio" %% "zio"       % "2.0.21",
+    "dev.zio" %% "zio"       % "2.1.17",
     "dev.zio" %% "zio-kafka" % "2.7.4"
   )
 
-  lazy val tapir: Seq[ModuleID] = Seq("com.softwaremill.sttp.tapir" %% "tapir-zio-http-server" % "1.10.0")
+  lazy val tapir: Seq[ModuleID] = Seq(
+    "com.softwaremill.sttp.tapir" %% "tapir-zio-http-server"   % tapirVersion,
+    "com.softwaremill.sttp.tapir" %% "tapir-json-zio"          % tapirVersion,
+    "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-bundle" % tapirVersion
+  )
 
   val application: Seq[ModuleID] = zio ++ tapir
   val client: Seq[ModuleID]      = config ++ zio
